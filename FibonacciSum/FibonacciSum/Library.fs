e00e11969c3fc41ed0a1a5f5005fbcc1516a83e8
olegshch <st069439@student.spbu.ru>
2020-06-06 16:42:48 +0300

    Update Library.fs

--- FibonacciSum/FibonacciSum/Library.fs
+++ FibonacciSum/FibonacciSum/Library.fs
@@ -2,15 +2,11 @@
 
  
 module FibSum =
+
     //Finds summ of even fibonaccies under 1000000
     let Counting =
-        let rec Summ i s =
-            let Fibonacci n =
-                let rec Fib a b i n =
-                    if i = n then a
-                    else Fib b (a + b) (i + 1) n
-                Fib 1 1 1 i
-            if (Fibonacci i) > 1000000 then s
-            elif (Fibonacci i) % 2 = 0 then Summ (i + 1) (s + Fibonacci i)
-            else Summ (i + 1) s
-        Summ 1 0
+        let rec fibonaccisumm previous current sum =
+            if current < 1000000 && current % 2 = 0 then fibonaccisumm current (previous + current) (sum + current)
+            elif current < 1000000 then fibonaccisumm current (previous + current) sum
+            else sum
+        fibonaccisumm 1 1 0
