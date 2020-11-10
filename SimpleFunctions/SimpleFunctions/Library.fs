f834a68042bb9ed5a7347ec9f59bb8dce7f6084d
olegshch <st069439@student.spbu.ru>
2020-11-11 01:40:59 +0300

    style fix

--- SimpleFunctions/SimpleFunctions/Library.fs
+++ SimpleFunctions/SimpleFunctions/Library.fs
@@ -1,40 +1,40 @@
 ﻿namespace SimpleFunctions
 
 module Functions =
-    let Factorial n =
-        let rec Fact s a b =
+    let factorial n =
+        let rec fact s a b =
             if a = b then s 
-            else Fact (s * (a + 1)) (a + 1) b
-        Fact 1 1 n 
+            else fact (s * (a + 1)) (a + 1) b
+        fact 1 1 n 
     
-    let Fibonacci n =
-        let rec Fib a b i n =
+    let fibonacci n =
+        let rec fib a b i n =
             if i = n then a
-            else Fib b (a + b) (i + 1) n
-        Fib 1 1 1 n
+            else fib b (a + b) (i + 1) n
+        fib 1 1 1 n
 
-    let ReverseList llist =
-        let rec Rev oldlist newlist = 
+    let reverseList llist =
+        let rec rev oldlist newlist = 
             match oldlist with
             | [] -> newlist
-            | head::tail -> Rev tail (head::newlist)
-        Rev llist []
+            | head::tail -> rev tail (head::newlist)
+        rev llist []
 
-    let PowerTwo n m =
-        let rec Power n m list = 
-            let Makingpower n =
+    let powerTwo n m =
+        let rec power n m list = 
+            let makingPower n =
                 let rec pow a b = 
                     if b = 0 then a
                     else pow (a * 2) (b - 1)
                 pow 1 n
             if n = m then list
-            else Power (n + 1) m (list @ [Makingpower n])
-        Power n (n + m + 1) []
+            else power (n + 1) m (list @ [makingPower n])
+        power n (n + m + 1) []
 
-    let Find list n =
-        let rec Findd list i =
+    let find list n =
+        let rec findd list i =
             match list with
             | [] -> -1
             | head::tail when head = n -> i
-            | _ -> Findd list.Tail (i + 1)
-        Findd list 0
+            | _ -> findd list.Tail (i + 1)
+        findd list 0
--- SimpleFunctions/TestsForFunctions/UnitTest1.fs
+++ SimpleFunctions/TestsForFunctions/UnitTest1.fs
@@ -2,23 +2,23 @@ module TestsForFunctions
 
 open NUnit.Framework
 open FsUnit
-
+open SimpleFunctions.Functions
 [<Test>]
 let ``Factorial test`` () =
-    SimpleFunctions.Functions.Factorial 5 |> should equal 120
+    factorial 5 |> should equal 120
 
 [<Test>]
 let ``Fibonacci test`` () =
-    SimpleFunctions.Functions.Fibonacci 10 |> should equal 55
+    fibonacci 10 |> should equal 55
 
 [<Test>]
 let ``Reverse test`` () =
-    SimpleFunctions.Functions.ReverseList [1;2;3;4;5] |> should equal [5;4;3;2;1]
+    reverseList [1; 2; 3; 4; 5] |> should equal [5; 4; 3; 2; 1]
 
 [<Test>]
 let ``Power test`` () =
-    SimpleFunctions.Functions.PowerTwo 4 7 |> should equal [16; 32; 64; 128; 256; 512; 1024; 2048]
+    powerTwo 4 7 |> should equal [16; 32; 64; 128; 256; 512; 1024; 2048]
 
 [<Test>]
 let ``Find test`` () =
-    SimpleFunctions.Functions.Find [3; 4; 5] 4 |> should equal 1
+    find [3; 4; 5] 4 |> should equal 1
