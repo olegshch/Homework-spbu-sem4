e9ad0e1e2949e011ca4d6cbd9717777f1f118f73
olegshch <st069439@student.spbu.ru>
2020-11-11 02:20:39 +0300

    making first two ex

---
+++ SecondFunctions/SecondFunctions/Library.fs
@@ -0,0 +1,30 @@
+﻿namespace SecondFunctions
+
+/// Функции для подсчета четных чисел в списке
+module EvenCount = 
+
+    /// Подсчет четных чисел в списке
+    let evenFilter =
+        List.filter (fun x -> x % 2 = 0) >> List.length
+
+    /// Подсчет четных чисел в списке
+    let evenMap =
+        List.map (fun x -> 1 - abs(x % 2)) >> List.fold (+) 0
+
+    /// Подсчет четных чисел в списке
+    let evenFold =
+        List.fold (fun acc x -> acc + 1 - abs(x % 2)) 0
+
+/// Map для дерева
+module MapTree =
+
+    /// Описание дерева
+    type Tree<'a> =
+        | Node of 'a * Tree<'a> * Tree<'a>
+        | Empty
+
+    /// Применение функции к дереву
+    let rec mapTree func tree = 
+        match tree with
+        | Node(h, l, r) -> Node(func h, mapTree func l, mapTree func r)
+        | Empty -> Empty
