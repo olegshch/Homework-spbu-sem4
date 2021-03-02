616245af09ef372951b2c10626c061996719ce48
olegshch <st069439@student.spbu.ru>
2021-03-02 19:37:41 +0300

    web surfer

---
+++ WebSurf/WebSurfTests/WebTests.fs
@@ -0,0 +1,15 @@
+module WebSurfTests
+
+open NUnit.Framework
+open FsUnit
+open WebSurf.WebSurf
+[<Test>]
+let ``simple test to check that function downloads links`` () =
+    let result = getAllLinks "https://fsharpforfunandprofit.com/"
+    result.Length |> should equal 2
+
+
+[<Test>]
+let ``test with wrong site`` () =
+    let result = getAllLinks "https://nonexistent_site"
+    result.IsEmpty |> should be True
