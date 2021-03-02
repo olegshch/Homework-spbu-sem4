0975af4ebd6a594b303c13b18565223827b4d6e4
olegshch <st069439@student.spbu.ru>
2021-03-02 19:53:55 +0300

    Update WebTests.fs

--- WebSurf/WebSurfTests/WebTests.fs
+++ WebSurf/WebSurfTests/WebTests.fs
@@ -8,6 +8,11 @@ let ``simple test to check that function downloads links`` () =
     let result = getAllLinks "https://fsharpforfunandprofit.com/"
     result.Length |> should equal 2
 
+[<Test>]
+let ``another test`` () =
+    let result = getAllLinks "https://www.google.com/"
+    result.Length |> should greaterThan 0
+
 
 [<Test>]
 let ``test with wrong site`` () =
